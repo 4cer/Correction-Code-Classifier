5bb457d65297235db9ec3c2812364e70b2cc6db6
Filipo <f.kozieniec@gmail.com>
2023-04-21 17:23:35 +0200

    czytanie z podfolderu po nazwie pliku

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import numpy as np
+import os
 
-def readFile():
+def readFile(file_name):
+
+    # Get the path of the root directory
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Define the subfolder containing the datasets
+    subfolder_name = 'dataset'
+
+    # Construct the path to the file
+    file_path = os.path.join(script_dir, subfolder_name, file_name)
+
     # read the contents of the file
-    with open('results.txt', 'r') as f:
+    with open(file_path, 'r') as f:
         content = f.read()
 
     # calculate the length of the string
@@ -16,13 +27,12 @@ def readFile():
 def readRange(data, start_idx, n):
     # check if the range of indices is within the bounds of the array
     if start_idx < 0 or start_idx + n > data.shape[0]:
-        #raise IndexError('Index out of bounds')
-        return -999
+        raise IndexError('Index out of bounds')
     else:
         # create a slice of the array containing n consecutive elements starting from the given index
         slice = data[start_idx:start_idx + n]
         return slice
 
 
-myArray = readFile()
+myArray = readFile("results.txt")
 print(readRange(myArray, 1, 300))
